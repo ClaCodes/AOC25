54972e1655a9a0670c3ba711aa49fced075bd0ea
Cla Mattia Galliard <clamattia@gmail.com>
2025-11-01 15:24:36 +0100

    AOC19 Day 01

--- src/day01.zig
+++ src/day01.zig
@@ -1,25 +1,62 @@
 const std = @import("std");
 
+fn required_fuel(mass: i32) i32 {
+    return @divFloor(mass, 3) - 2;
+}
+
+fn rocket_limit(mass: i32) i32 {
+    const fuel: i32 = required_fuel(mass);
+    if (fuel < 0) {
+        return 0;
+    } else {
+        return fuel + rocket_limit(fuel);
+    }
+}
+
 pub fn solve(reader: *std.Io.Reader, writer: *std.Io.Writer) !void {
+    var total: i32 = 0;
+    var limit: i32 = 0;
     while (reader.takeDelimiterExclusive('\n')) |line| {
         reader.toss(1);
-        try writer.print("{s}\n", .{line});
+        const mass = try std.fmt.parseInt(i32, line, 10);
+        total += required_fuel(mass);
+        limit += rocket_limit(mass);
     } else |err| switch (err) {
         error.EndOfStream => {},
         error.StreamTooLong,
         error.ReadFailed,
         => |e| return e,
     }
+    try writer.print("{d}\n", .{total});
+    try writer.print("{d}\n", .{limit});
     try writer.flush();
+}
+
+test "required_fuel" {
+    try std.testing.expectEqual(required_fuel(12), 2);
+    try std.testing.expectEqual(required_fuel(14), 2);
+    try std.testing.expectEqual(required_fuel(1969), 654);
+    try std.testing.expectEqual(required_fuel(100756), 33583);
+}
+
+test "rocket_limit" {
+    try std.testing.expectEqual(rocket_limit(12), 2);
+    try std.testing.expectEqual(rocket_limit(14), 2);
+    try std.testing.expectEqual(rocket_limit(1969), 966);
+    try std.testing.expectEqual(rocket_limit(100756), 50346);
 }
 
 test "solve" {
     const in =
-        \\TEST
+        \\12
+        \\14
+        \\1969
+        \\100756
         \\
     ;
     const out =
-        \\TEST
+        \\34241
+        \\51316
         \\
     ;
     var buf: [255]u8 = undefined;
